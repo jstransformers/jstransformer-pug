076f60bb688cac4d3c4b3f38574abe9c28b69c4a
Timothy Gu <timothygu99@gmail.com>
2015-02-11 21:08:41 -0500

    Add compileFile and compileFileClient

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var jade = require('jade');
+var fs   = require('fs');
 
 exports.name = 'jade';
 exports.outputFormat = 'html';
@@ -10,4 +11,15 @@ exports.compile = function (source, options) {
 };
 exports.compileClient = function (source, options) {
   return jade.compileClientWithDependenciesTracked(source, options);
+};
+exports.compileFile = function (path, options) {
+  var fn = jade.compileFile(path, options);
+  return {fn: fn, dependencies: fn.dependencies}
+};
+exports.compileFileClient = function (path, options) {
+  // There is no compileFileClientWithDependenciesTracked so gotta do it
+  // manually.
+  options = options || {};
+  options.filename = options.filename || path;
+  return exports.compileClient(fs.readFileSync(path, 'utf8'), options);
 };
